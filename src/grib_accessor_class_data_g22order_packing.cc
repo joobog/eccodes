5a080bad5225e372227fa3a209998e1bc1770cac
Eugen Betke <eugen.betke@ecmwf.int>
2023-03-29 13:15:19 +0000

    ECC-1472: Cleanup + single-precision

--- src/grib_accessor_class_data_g22order_packing.cc
+++ src/grib_accessor_class_data_g22order_packing.cc
@@ -18,7 +18,7 @@
    SUPER      = grib_accessor_class_values
    IMPLEMENTS = init
    IMPLEMENTS = pack_double
-   IMPLEMENTS = unpack_double
+   IMPLEMENTS = unpack_double; unpack_float
    IMPLEMENTS = unpack_double_element;unpack_double_element_set
    IMPLEMENTS = value_count
    MEMBERS=const char*  numberOfValues
@@ -57,6 +57,7 @@ or edit "accessor.class" and rerun ./make_class.pl
 */
 
 static int pack_double(grib_accessor*, const double* val, size_t* len);
+static int unpack_double(grib_accessor*, double* val, size_t* len);
 static int value_count(grib_accessor*, long*);
 static void init(grib_accessor*, const long, grib_arguments*);
 static void init_class(grib_accessor_class*);
@@ -120,7 +121,7 @@ static grib_accessor_class _grib_accessor_class_data_g22order_packing = {
     0,                  /* grib_pack procedures long */
     0,                /* grib_unpack procedures long */
     &pack_double,                /* grib_pack procedures double */
-    0,              /* grib_unpack procedures double */
+    &unpack_double,              /* grib_unpack procedures double */
     0,                /* grib_pack procedures string */
     0,              /* grib_unpack procedures string */
     0,          /* grib_pack array procedures string */
@@ -159,7 +160,6 @@ static void init_class(grib_accessor_class* c)
     c->is_missing    =    (*(c->super))->is_missing;
     c->pack_long    =    (*(c->super))->pack_long;
     c->unpack_long    =    (*(c->super))->unpack_long;
-    c->unpack_double    =    (*(c->super))->unpack_double;
     c->pack_string    =    (*(c->super))->pack_string;
     c->unpack_string    =    (*(c->super))->unpack_string;
     c->pack_string_array    =    (*(c->super))->pack_string_array;
--- tests/grib_ecc-1472.sh
+++ tests/grib_ecc-1472.sh
@@ -18,47 +18,74 @@ label="grib_ecc-1472_test"
 #export ECCODES_DEBUG=1
 #rm -f $temp
 
-echo $PWD
+
 GRIB_COPY="${tools_dir}/grib_copy"
 GRIB_SET="${tools_dir}/grib_set"
 GRIB_LS="${tools_dir}/grib_ls"
 
-INFN="${data_dir}/gfs.complex.mvmu.grib2"
-OUTFN1="${data_dir}/gfs.complex.mvmu.grib2_1.tmp"
-OUTFN2="${data_dir}/gfs.complex.mvmu.grib2_2.tmp"
-#gfs.complex.mvmu.grib2
 
-COMMANDS=( 
-  "$GRIB_COPY -r ${INFN} ${OUTFN1}" # OK
-  "$GRIB_COPY -r ${OUTFN1} ${OUTFN2}" # OK
-  "$GRIB_SET -r ${INFN} ${OUTFN1}" # OK
-  "$GRIB_SET -rs bitmapPresent=1 ${INFN} ${OUTFN1}" # OK
-  "$GRIB_SET -rs bitsPerValue=16 ${INFN} ${OUTFN1}"  # OK
-  #"$GRIB_SET -s bitsPerValue=16 ${INFN} ${OUTFN1}"  # FAILS
-  #"$GRIB_SET -s packingType=grid_simple ${INFN} ${OUTFN1}" # FAILS: unpack_double() uses grid_simple accessors instead of grid_complex
-  #"$GRIB_SET -rs bitmapPresent=1,packingType=grid_simple ${INFN} ${OUTFN1}" # FAILS: unpack_double() uses grid_simple accessors instead of grid_complex
-  #"$GRIB_SET -s scaleValuesBy=0.98 gfs.complex.mvmu.grib2 test.grib"
-  #"$GRIB_SET -r -s packingType=grid_simple /perm/mod/public/eb/cams_ml1_so3_202301100.grib ${OUTFN1}" # OK
-  #"$GRIB_SET -r -s packingType=grid_simple /perm/mod/public/eb/cams_ml1_aermr02_202301100.grib ${OUTFN1}" # OK
-)
+function die() {
+  echo "ERROR: $1"
+  exit 1
+}
 
-for ((i = 0; i < ${#COMMANDS[@]}; i++)); do
-  COMMAND="${COMMANDS[$i]}"
-  echo "COMMAND $COMMAND"
-  $COMMAND
-  output1=$($GRIB_LS -n statistics $INFN | head -n 3 | tail -n 1)
-  output2=$($GRIB_LS -n statistics $OUTFN1 | head -n 3 | tail -n 1)
+
+function check_equality() {
+  $fn_1=$1
+  $fn_2=$2
+  output1=$($GRIB_LS -n statistics $fn_1 | head -n 3 | tail -n 1)
+  output2=$($GRIB_LS -n statistics $fn_2 | head -n 3 | tail -n 1)
   if [ "$output1" != "$output2" ]; then
+    echo "Data loss detected"
     echo "OUTPUT1: $output1"
     echo "OUTPUT2: $output2"
     return 1
   fi
+  return 0
+}
+
+
+cleanup() {
+  echo "Cleaning up"
+  rm -f ${OUTFN1} ${OUTFN2}
+}
+
+trap cleanup EXIT
+
+INFNS=( 
+"${data_dir}/gfs.complex.mvmu.grib2"
+"${data_dir}/cams_ml1_so3_202301100.grib "
+"${data_dir}/cams_ml1_aermr02_202301100.grib"
+)
+
+for INFN in "${INFNS[@]}"; do
+  echo "Processing $INFN"
+
+  OUTFN1="${INFN}_1.tmp"
+  OUTFN2="${INFN}_2.tmp"
+
+  $GRIB_COPY -r ${INFN} ${OUTFN1} | die "Copy1 failed"
+  check_equality $INFN $OUTFN1 | die "Equality check failed"
+
+  $GRIB_COPY -r ${OUTFN1} ${OUTFN2} | die "Copy2 failed"
+  check_equality $OUTFN1 $OUTFN2 | die "Equality check failed"
+
+  $GRIB_SET -r ${INFN} ${OUTFN1} | die "grib_set failed"
+  check_equality $INFN $OUTFN1 | die "Equality check failed"
+
+  "$GRIB_SET -rs bitmapPresent=1 ${INFN} ${OUTFN1}" | "grib_set failed"
+  check_equality $INFN $OUTFN1 | die "Equality check failed"
+
+  "$GRIB_SET -rs bitsPerValue=16 ${INFN} ${OUTFN1}"
+  check_equality $INFN $OUTFN1 | die "Equality check failed"
+
+  cleanup
 done
 
-# Clean up
-if [ -e "$OUTFN1" ]; then
-  rm $OUTFN1
-fi
-if [ -e "$OUTFN2" ]; then
-  rm $OUTFN2
-fi
+# Failing commands
+#"$GRIB_SET -s bitsPerValue=16 ${INFN} ${OUTFN1}"  # FAILS
+#"$GRIB_SET -s packingType=grid_simple ${INFN} ${OUTFN1}" # FAILS: unpack_double() uses grid_simple accessors instead of grid_complex
+#"$GRIB_SET -rs bitmapPresent=1,packingType=grid_simple ${INFN} ${OUTFN1}" # FAILS: unpack_double() uses grid_simple accessors instead of grid_complex
+#"$GRIB_SET -s scaleValuesBy=0.98 gfs.complex.mvmu.grib2 test.grib"
+#"$GRIB_SET -r -s packingType=grid_simple /perm/mod/public/eb/cams_ml1_so3_202301100.grib ${OUTFN1}" # OK
+#"$GRIB_SET -r -s packingType=grid_simple /perm/mod/public/eb/cams_ml1_aermr02_202301100.grib ${OUTFN1}" # OK
